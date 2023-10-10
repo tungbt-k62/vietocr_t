53e6df23445f6160aa430fda3ad627d148c23bcf
Bui Tung <73340756+tungbt-k62@users.noreply.github.com>
2023-10-10 14:21:40 +0000

    Update cnn.py

--- vietocr/model/backbone/cnn.py
+++ vietocr/model/backbone/cnn.py
@@ -1,19 +1,21 @@
 import torch
 from torch import nn
-
+import timm
 import vietocr.model.backbone.vgg as vgg
 from vietocr.model.backbone.resnet import Resnet50
 
 class CNN(nn.Module):
     def __init__(self, backbone, **kwargs):
         super(CNN, self).__init__()
-
         if backbone == 'vgg11_bn':
             self.model = vgg.vgg11_bn(**kwargs)
         elif backbone == 'vgg19_bn':
             self.model = vgg.vgg19_bn(**kwargs)
         elif backbone == 'resnet50':
             self.model = Resnet50(**kwargs)
+        elif backbone == 'timm_backone':
+            print('timm')
+            self.model = timm.create_model(**kwargs)
 
     def forward(self, x):
         return self.model(x)
