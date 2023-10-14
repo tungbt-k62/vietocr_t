767d839dbe4684dbfaf643a48ee9a0117c4133e1
Bui Tung <73340756+tungbt-k62@users.noreply.github.com>
2023-10-14 03:28:13 +0000

    Update cnn.py

--- vietocr/model/backbone/cnn.py
+++ vietocr/model/backbone/cnn.py
@@ -4,6 +4,8 @@ import torch.nn.functional as F
 import timm
 import vietocr.model.backbone.vgg as vgg
 from vietocr.model.backbone.resnet import Resnet50
+import torchvision
+from collections import OrderedDict
 
 class CNN(nn.Module):
     def __init__(self, backbone, **kwargs):
@@ -18,36 +20,40 @@ class CNN(nn.Module):
             # print('timm')
             self.model = timm.create_model(**kwargs)
             self.timm_chans = self.model.feature_info.channels()
-            self.conv4 = nn.Conv2d(self.timm_chans[4], 256, 1)
-            self.conv3 = nn.Conv2d(self.timm_chans[3], 256, 1)
-            self.conv2 = nn.Conv2d(self.timm_chans[2], 256, 1)
-            # self.conv1 = nn.Conv2d(self.timm_chans[1], self.timm_chans[4], 1)
+            self.fpn = torchvision.ops.FeaturePyramidNetwork([self.timm_chans[2], self.timm_chans[3], self.timm_chans[4]], 256)
+            # self.conv4 = nn.Conv2d(self.timm_chans[4], 256, 1)
+            # self.conv3 = nn.Conv2d(self.timm_chans[3], 256, 1)
+            # self.conv2 = nn.Conv2d(self.timm_chans[2], 256, 1)
         self.backbone = backbone
 
     def forward(self, x):
         if self.backbone == 'timm_backbone':
             xs =  self.model(x)
-            xs[4] = self.conv4(xs[4])
-            xs[4] = F.relu(xs[4])
-            xs[4] = F.interpolate(xs[4], size=xs[2].shape[2:], mode='bilinear', align_corners=False)
+            # xs[4] = self.conv4(xs[4])
+            # xs[4] = F.relu(xs[4])
+            # xs[4] = F.interpolate(xs[4], size=xs[2].shape[2:], mode='bilinear', align_corners=False)
             
-            xs[3] = self.conv3(xs[3])
-            xs[3] = F.relu(xs[3])
-            xs[3] = F.interpolate(xs[3], size=xs[2].shape[2:], mode='bilinear', align_corners=False)
+            # xs[3] = self.conv3(xs[3])
+            # xs[3] = F.relu(xs[3])
+            # xs[3] = F.interpolate(xs[3], size=xs[2].shape[2:], mode='bilinear', align_corners=False)
 
-            xs[2] = self.conv2(xs[2])
-            xs[2] = F.relu(xs[2])
-            # xs[2] = F.interpolate(xs[2], scale_factor=2, mode='bilinear', align_corners=False)
-
-            # xs[1] = self.conv1(xs[1])
-            # xs[1] = F.relu(xs[1])
-
-            x = xs[2]+xs[3]+xs[4]
+            # xs[2] = self.conv2(xs[2])
+            # xs[2] = F.relu(xs[2])
             
+            # x = xs[2]+xs[3]+xs[4]
+
+            x = OrderedDict()
+            x['feat2'] = xs[2]
+            x['feat3'] = xs[3]
+            x['feat4'] = xs[4]
+            x = self.fpn(x)
+            x = x['feat2']
+
             x = x.transpose(-1, -2)
             x = x.flatten(2)
             x = x.permute(-1, 0, 1)
             return x
+        
         else:
             return self.model(x)
 
